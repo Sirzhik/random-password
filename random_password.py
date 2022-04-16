2de3e8876f2aca5ad72538bc9bb5ad62d2eea380
Skoder <86963273+Sirzhik@users.noreply.github.com>
2022-04-16 15:14:43 +0300

    Update random_password.py

--- random_password.py
+++ random_password.py
@@ -8,7 +8,7 @@ sym1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o
 num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 password = sym[randint(0, 25)] + num[randint(0, 9)] + sym[randint(0, 25)] + sym1[randint(0, 25)] + sym[randint(0, 25)] + sym[randint(0, 25)] + num[randint(0, 9)] + sym1[randint(0, 25)]
 print(password)
-print('Your password: /usr/bin/' + name)
+print('Your random password: ' + name)
 file.write(password)
 file.close()
 
